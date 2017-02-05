1e75932960d4c575b500612ec77a81f269c8b37d
Ioannis Kokkinis <info@upg.gr>
2017-02-05 17:39:35 +0200

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+sudo apt-get install build-essential git cmake libboost1.55-all-dev
+rm -rf ./xmdcoin
+rm -rf xmdcoin
+rm /etc/init/xmadcoind.conf
+git clone https://github.com/upggr/xmd-coin.git xmdcoin
+cd xmdcoin
+cp xmadcoind.conf /etc/init/xmadcoind.conf
+make
+cd build
+cd release
+cd src
+chmod +x xmdcoind
+start xmadcoind
