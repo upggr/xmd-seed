8fe131054a10f0a2aa6f13dac323cefcdc430990
Ioannis Kokkinis <info@upg.gr>
2017-02-05 23:47:39 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,6 +3,8 @@ sudo apt-get install build-essential git cmake libboost1.55-all-dev
 stop xmadcoind
 rm -rf .xmdcoin
 rm -rf xmdcoin
+rm /home/azureuser/xmdcoin/build/release/src/xmdcoind
+rm /home/azureuser/xmdcoin/build/release/src/simplewallet
 rm /etc/init/xmadcoind.conf
 git clone https://github.com/upggr/xmd-coin.git xmdcoin
 cd xmdcoin
@@ -12,4 +14,6 @@ cd build
 cd release
 cd src
 chmod +x xmdcoind
+cp /home/azureuser/xmdcoin/build/release/src/xmdcoind /home/azureuser/xmdcoind
+cp /home/azureuser/xmdcoin/build/release/src/simplewallet /home/azureuser/simplewallet
 start xmadcoind
